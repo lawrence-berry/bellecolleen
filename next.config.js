d947b172062a9e36a00176dbbe50447b59ce8e20
Lawrence Berry <lawrence.berry@gmail.com>
2025-04-13 20:00:25 -0300

    Deployment

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ const nextConfig = {
   images: {
     unoptimized: true,  // Disable image optimization for static export
   },
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
 };
 
 module.exports = nextConfig; 
